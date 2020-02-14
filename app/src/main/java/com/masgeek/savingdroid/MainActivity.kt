b4ccd72ac3382839d01bf1d34aac3645c832b589
Sammy Barasa <barsamms@gmail.com>
2020-02-14 09:59:41 +0300

    Second commit

---
+++ app/src/main/java/com/masgeek/savingdroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.masgeek.savingdroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
